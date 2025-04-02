d8480a916f9e9c9f74e9297dc14c1f89b577da7b
phillip lewis <philliplewis910@gmail.com>
2025-04-02 16:05:20 -0500

    adding js components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import LandingPage from './Components/Landing_Page/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div className='app'>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={LandingPage} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+        <Route path="/appointments" element={<h1>Appointments</h1>} />
+        </Routes>
+        </BrowserRouter>
+      </div>
+      </>
   );
 }
 
